2650bbb9001c69ba6b78474dfb1feca55908c2b3
Arya Augusta <aryaaugusta@gmail.com>
2024-02-21 21:18:44 +0700

    multiples 3 or 5

--- 13-multiples3or5.js
+++ 13-multiples3or5.js
@@ -2,10 +2,12 @@ function solution(number) {
     let sum = 0
     for (let i = 0; i < number; i++) {
         if (i % 3 === 0 || i % 5 === 0) {
+            console.log(sum)
+            console.log(i)
             sum += i;
         }
     }
     return sum
 }
 
-console.log(solution(200))
+console.log(solution(10))
